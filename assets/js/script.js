335a01788c44fac56c6a727555ed587da8995f70
lverghese <loganverghese@gmail.com>
2021-07-21 21:55:08 -0400

    comments for structure

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Need a function with the current date at the top
+
+
+
+
+// function for the color coding of the timeblocks
+// if its the current hour set it to present css style
+// if its before the current hour it is the past css style
+// if its after the current hour make it future css style
+// anything else make white
+
+
+// edit task function
+
+
+// save task function
+
+
+// local storage needs to save events in scheduler on screen after refresh
